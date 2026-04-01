84be30281cefa51629749bae2dbdcf8f2774b182
0XFANGO <silencerichard@163.com>
2026-04-01 17:57:24 +0800

    feat: export new episode, user, settings, speaker types

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,35 @@ export type {ClientOptions} from './types/client.js';
 export type {ConnectInitResponse, TokenResponse, StoredCredentials} from './types/auth.js';
 export type {ApiKeyResponse} from './types/settings.js';
 export type {CheckinResponse, CheckinStatusResponse} from './types/checkin.js';
+export type {
+	ContentSource,
+	EpisodeSpeaker,
+	EpisodeCreator,
+	CreateEpisodeResponse,
+	CreatePodcastParams,
+	CreateSpeechParams,
+	CreateExplainerVideoParams,
+	CreateSlidesParams,
+	EpisodeItem,
+	EpisodeDetail,
+	ListEpisodesParams,
+	ListEpisodesResponse,
+	DeleteEpisodesParams,
+	EpisodeDetailTopicData,
+	EpisodeDetailAudio,
+	EpisodeDetailVideo,
+	EpisodeDetailPage,
+	EpisodeDetailPreprocess,
+	EpisodeDetailReference,
+	EpisodeDetailScript,
+	EpisodeDetailSeoMeta,
+} from './types/episodes.js';
+export type {UserProfile, SubscriptionInfo, SubscriptionPlan} from './types/users.js';
+export type {
+	SettingsItem,
+	SettingsResponse,
+	SettingsSpeaker,
+	StyleImage,
+	UpdateEpisodeConfigParams,
+} from './types/settings.js';
+export type {Speaker, ListSpeakersParams, ListSpeakersResponse} from './types/speakers.js';
